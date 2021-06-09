74fd533d85158576adc875e8f9212ea0f7485e3a
Kaseem Bradley <kaseembradley@Meryls-phone.localdomain>
2021-06-09 13:00:47 -0400

    wrote query that displays name and quantity of the products ordered in order with id 10251

--- queries.sql
+++ queries.sql
@@ -8,6 +8,17 @@ join category c
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+ select [Order].Id, CompanyName, OrderDate from [Order]
+    left join Shipper on EmployeeId=Shipper.id
+    where OrderDate < '2012-08-09';
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+
+select p.productname,
+o.quantity
+from orderDetail o
+join product p
+on p.id = o.productid
+where orderId = 10251;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
